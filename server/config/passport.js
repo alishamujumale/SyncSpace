84f7e2d2a28ded8b4e773f80dd2c1f15a45dc360
alishamujumale <alishamujumale@gmail.com>
2026-07-17 16:21:42 +0530

    deployment fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
+const User = require('../models/user');
 
 const defaultPort = process.env.PORT || 10000;
 const defaultCallback = `http://localhost:${defaultPort}/auth/google/callback`;
